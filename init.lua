509ffecd55e05ca46cc87f9ec4e9bfb8f2eb1e68
Bonono63 <bonoria63@gmail.com>
2024-05-21 12:15:47 -0400

    Update init.lua

--- init.lua
+++ init.lua
@@ -4,3 +4,39 @@ vim.opt.number=true
 vim.opt.relativenumber=true
 vim.opt.tabstop=4
 vim.opt.shiftwidth=4
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  -- bootstrap lazy.nvim
+  -- stylua: ignore
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+end
+vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
+
+require("lazy").setup({
+    spec = {
+        {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+        {"ribru17/bamboo.nvim", lazy = false, priority = 1000,
+        config = function()
+            require("bamboo").setup {
+            }
+            require("bamboo").load()
+        end}
+    },
+    performance = {
+    rtp = {
+      -- disable some rtp plugins
+      disabled_plugins = {
+        "gzip",
+        -- "matchit",
+        -- "matchparen",
+        -- "netrwPlugin",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
+    },
+  },
+})

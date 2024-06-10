b3e13a0d9b3de4295f4ee5a9fc80c913e98f71fa
Bonono63 <bonoria63@gmail.com>
2024-06-10 00:15:37 -0400

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,9 +1,9 @@
-vim.opt.backup=false
-vim.opt.expandtab=true
-vim.opt.number=true
-vim.opt.relativenumber=true
-vim.opt.tabstop=4
-vim.opt.shiftwidth=4
+vim.opt.backup = false
+vim.opt.expandtab = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
@@ -15,28 +15,40 @@ end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
 require("lazy").setup({
-    spec = {
-        {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
-        {"ribru17/bamboo.nvim", lazy = false, priority = 1000,
-        config = function()
-            require("bamboo").setup {
-            }
-            require("bamboo").load()
-        end}
-    },
-    performance = {
-    rtp = {
-      -- disable some rtp plugins
-      disabled_plugins = {
-        "gzip",
-        -- "matchit",
-        -- "matchparen",
-        -- "netrwPlugin",
-        "tarPlugin",
-        "tohtml",
-        "tutor",
-        "zipPlugin",
-      },
-    },
-  },
+	spec = {
+		{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+		{
+			"ribru17/bamboo.nvim",
+			lazy = false,
+			priority = 1000,
+			config = function()
+				require("bamboo").setup({})
+				require("bamboo").load()
+			end,
+		},
+		{ "williamboman/mason.nvim" },
+		{ "williamboman/mason-lspconfig.nvim" },
+		{ "neovim/nvim-lspconfig" },
+	},
+	performance = {
+		rtp = {
+			-- disable some rtp plugins
+			disabled_plugins = {
+				"gzip",
+				-- "matchit",
+				-- "matchparen",
+				-- "netrwPlugin",
+				"tarPlugin",
+				"tohtml",
+				"tutor",
+				"zipPlugin",
+			},
+		},
+	},
 })
+
+require("mason").setup()
+require("mason-lspconfig").setup({
+	ensure_installed = { "lua_ls" },
+})
+require("lspconfig").lua_ls.setup({})
